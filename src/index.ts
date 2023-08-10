fec770b93f45c44800feba354fb6b6770ff4a1bc
Brian Cross <briancross@shaw.ca>
2023-08-10 07:28:17 -0600

    Switch to Mediastack API.

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,28 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const { API_KEY, API_BASE_URL } = process.env;
 
+// https://github.com/chimurai/http-proxy-middleware/issues/293#issuecomment-449548863
+const updateQueryStringParameter = (
+  path: string,
+  key: string,
+  value: string
+) => {
+  const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  const separator = path.indexOf("?") !== -1 ? "&" : "?";
+  if (path.match(re)) {
+    return path.replace(re, "$1" + key + "=" + value + "$2");
+  } else {
+    return path + separator + key + "=" + value;
+  }
+};
+
 const proxy = createProxyMiddleware({
   target: API_BASE_URL,
   changeOrigin: true,
-  pathRewrite: { "^/api": "/" },
-  headers: {
-    Authorization: API_KEY!,
+  pathRewrite: function (path, _req) {
+    let newPath = path;
+    newPath = updateQueryStringParameter(newPath, "access_key", API_KEY!);
+    return newPath;
   },
 });
 
@@ -24,6 +40,6 @@ app.use(cors());
 
 app.use(helmet());
 
-app.use("/api", proxy);
+app.use("/", proxy);
 
 app.listen(PORT, () => console.log(`Server listing on port ${PORT}`));
